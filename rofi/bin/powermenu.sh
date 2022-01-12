7b63e30e0efc23bd0a9f0724a2136ceb1bdd7424
Thomas Guillouet <thomas.guillouet@outlook.com>
2022-01-12 22:44:34 +0100

    Rofi power menu

--- rofi/bin/powermenu.sh
+++ rofi/bin/powermenu.sh
@@ -1,6 +1,29 @@
 rofi_command="rofi -theme ~/.config/rofi/powermenu.rasi"
 
-options="Shutdown\nReboot\nlock\nSuspend\nLogout"
+shutdown=""
+reboot=""
+lock=""
+logout=""
 
-echo -e "$options" | $rofi_command -dmenu
+# Session related stuff
+user=$(whoami)
+session="$(loginctl | grep $user)"
+session_parts=($session)
 
+options="$shutdown\n$reboot\n$lock\n$logout"
+
+choice="$(echo -e "$options" | $rofi_command -dmenu)"
+
+case $choice in
+	$shutdown)
+		systemctl shutdown
+	;;
+	$reboot)
+		systemctl reboot
+	;;
+	$lock)
+		loginctl lock-session ${session_parts[0]}
+	;;
+	$logout)
+		loginctl kill-session ${session_parts[0]}
+esac
