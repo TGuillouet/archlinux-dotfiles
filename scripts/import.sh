7129d1583233bb8e6e4a34cf76bf5950f1d723a4
Thomas Guillouet <thomas.guillouet@outlook.com>
2022-01-14 16:35:08 +0100

    Import script

---
+++ scripts/import.sh
@@ -0,0 +1,10 @@
+mkdir -p ~/.config/{bspwm,sxhkd,polybar,rofi,fish,ncmpcpp,mpd}
+
+cp -rf  ~/.dotfiles/bspwm/ ~/.config/bspwm/
+cp -rf ~/.dotfiles/sxhkd/ ~/.config/sxhkd/
+cp -rf ~/.dotfiles/polybar/ ~/.config/polybar/
+cp -rf ~/.dotfiles/rofi/ ~/.config/rofi/
+cp -rf ~/.dotfiles/fish/ ~/.config/fish/
+cp -rf ~/.dotfiles/ncmpcpp/ ~/.config/ncmpcpp/ 
+cp -rf ~/.dotfiles/mpd/ ~/.config/mpd/ 
+
